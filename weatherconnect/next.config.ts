8ef60ba67c93668a3f6cc1bde88c0f5dd4215175
yuyuyubin <129951226+yuyuyubin@users.noreply.github.com>
2024-12-07 20:52:19 +0900

    asdf

--- weatherconnect/next.config.ts
+++ weatherconnect/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 중 ESLint 오류를 무시
+  },
+  typescript: {
+    ignoreBuildErrors: true, // 빌드 중 TypeScript 오류를 무시
+  },
+  reactStrictMode: true, // React Strict Mode 활성화
+  swcMinify: true, // SWC 기반의 빠른 Minify 활성화
 };
 
 export default nextConfig;
